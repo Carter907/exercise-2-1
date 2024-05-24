fe5ae48bf70b26b27391e6d7954184af5efa306f
Carter S <speers.carter@gmail.com>
2024-05-24 00:39:21 -0400

    unit test 2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ public class Program
     public static void Main(String[] args)
     {
         Console.WriteLine("enter inches");
-        var inches = 5;
+        var inches = 50;
         double centimeters = inches * unitFactor;
         Console.WriteLine($"{inches} inches is {centimeters} centimeters");
     }
